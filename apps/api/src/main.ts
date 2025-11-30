72c21994acbf121496851c297c0c93be26737e43
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-11-30 20:53:23 +0200

    wip

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<void> {
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (error) {
+    console.error(error);
+  }
 }
-bootstrap();
+
+await bootstrap();

964e51b161078a5309b5500582cf8d2c571dfd6f
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-12-02 15:50:12 +0200

    feat: add users crud operation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TenantModule } from './tenant/tenant.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
     TenantModule,
+    UsersModule,
   ],
 })
 export class AppModule {}

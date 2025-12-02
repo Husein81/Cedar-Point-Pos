206776f939a360810e77736902f0ccfb6c5a1317
Hx8i <Hashemnasrallah2005@gmail.com>
2025-12-02 16:46:34 +0200

    SCRUM-16: implement role-based access middleware

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { TenantModule } from './tenant/tenant.module';
 import { AuthModule } from './auth/auth.module';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Module({
   imports: [
@@ -10,6 +12,12 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
     TenantModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}

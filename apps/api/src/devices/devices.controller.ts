85e4942e2381b9e0d5d2ee34f9c5ea05cd7b4fc5
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-12-04 10:26:24 +0200

    feat:implement pos device registration

---
+++ apps/api/src/devices/devices.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Put, Req } from '@nestjs/common';
+import type { Request } from 'express';
+import { DevicesService } from './devices.service';
+import { Prisma } from '@repo/db';
+
+@Controller('devices')
+export class DevicesController {
+  constructor(private readonly devicesService: DevicesService) {}
+
+  @Post('register')
+  registerDevice(@Req() request: Request) {
+    const { tenantId } = request.user as { tenantId: string };
+    const body = request.body as Prisma.POSDeviceUncheckedCreateInput;
+    return this.devicesService.registerDevice(tenantId, body);
+  }
+
+  @Put('update-active/:id')
+  updateActiveDevice(@Req() request: Request) {
+    const deviceId = request.params.id;
+
+    if (!deviceId) {
+      throw new Error('Device ID is required');
+    }
+
+    const { isActive } = request.body as { isActive: boolean };
+    return this.devicesService.updateActiveDevice(deviceId, isActive);
+  }
+
+  @Put('update-kds-flag/:id')
+  updateKdsFlag(@Req() request: Request) {
+    const deviceId = request.params.id;
+    if (!deviceId) {
+      throw new Error('Device ID is required');
+    }
+    const { isKDS } = request.body as { isKDS: boolean };
+    return this.devicesService.updateKdsFlag(deviceId, isKDS);
+  }
+
+  @Get('validate-token')
+  validateDeviceToken(@Req() request: Request) {
+    const token = request.headers['x-device-token'] as string;
+    return this.devicesService.validateDeviceToken(token);
+  }
+}
---
+++ apps/api/src/devices/devices.service.ts
@@ -0,0 +1,43 @@
+import { generateToken } from '@/utils/generateToken';
+import { Injectable } from '@nestjs/common';
+import { Prisma, prisma } from '@repo/db';
+
+@Injectable()
+export class DevicesService {
+  async registerDevice(
+    tenantId: string,
+    data: Prisma.POSDeviceUncheckedCreateInput,
+  ) {
+    const token = generateToken();
+
+    const device = await prisma.pOSDevice.create({
+      data: {
+        ...data,
+        token,
+        tenantId: tenantId,
+      },
+    });
+
+    return device;
+  }
+
+  async updateActiveDevice(deviceId: string, isActive: boolean) {
+    return await prisma.pOSDevice.update({
+      where: { id: deviceId },
+      data: { isActive },
+    });
+  }
+
+  async updateKdsFlag(deviceId: string, isKDS: boolean) {
+    return await prisma.pOSDevice.update({
+      where: { id: deviceId },
+      data: { isKDS },
+    });
+  }
+
+  async validateDeviceToken(token: string) {
+    return await prisma.pOSDevice.findUnique({
+      where: { token },
+    });
+  }
+}

33de85ed74c854ac4cf1525cc8760ff22842aaaf
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-12-17 23:05:20 +0200

    wip

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,10 +4,12 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard.js';
 import { RolesGuard } from './common/guards/roles.guard.js';
 import { PrismaService } from './prisma.service.js';
+import { AppController } from './app.controller.js';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   providers: [
+    AppController,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,

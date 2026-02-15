72fefe2544ac368e63e00b02c013ecdf531c0f77
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2026-02-15 17:52:05 +0200

    WIP

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,19 +1,24 @@
 import Provider from "@/components/provider";
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "../global.css";
+import { useThemeStore } from "@/store/theme";
+
 export default function RootLayout() {
+  const { theme, isDark } = useThemeStore();
   return (
     <Provider>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
           options={{ presentation: "modal", title: "Modal" }}
         />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style={isDark ? "light" : "dark"} />
+      <PortalHost />
     </Provider>
   );
 }
---
+++ apps/mobile/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Badge } from "./badge";
+export { Button } from "./button";
+export { Icon } from "./icon";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Switch } from "./switch";
+export { Textarea } from "./textarea";
